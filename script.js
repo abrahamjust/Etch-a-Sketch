bf3c12511a5ac5b675519c14df573e221c9c7bee
Abraham Justin <162578329+abrahamjust@users.noreply.github.com>
2024-10-28 08:05:15 +0000

    made the grid resizeable according to user input

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// global variables and the default state.
+
+let a = 10;
+createGrid(a);
+
+// functions
+
+// creating the grid
 function createGrid(a) {
     const container = document.querySelector('.Container');
     for(i = 0; i < a; i++) {
@@ -10,14 +18,43 @@ function createGrid(a) {
         }
         container.appendChild(rowDiv);
     }
+    // activate the hover effect after creating the grid
+    activateHoverEffect();
 }
 
-a = 10;
-createGrid(a);
+// getting user input
+function alertSizeInput() {
+    do {
+        a = prompt("Enter the size of the grid (max size = 100, min size > 0)");
+    } while(a < 0 || a > 100);
+    resetGrid();
+    createGrid(a);
+}
 
-divColor = document.querySelectorAll('.columnDiv');
-divColor.forEach(divColor => {
-    divColor.addEventListener("mouseover", function (e) {
-        e.target.style.backgroundColor = "Black";
-      });    
-});
+// deleting the parent node(rowDiv) will delete the child nodes(columnDiv) also
+function resetGrid() {
+    const rowDiv = document.querySelectorAll('.rowDiv');
+    rowDiv.forEach(rowDiv => {
+        rowDiv.remove();
+    });
+}
+
+// reason why i put this inside a function is because if i don't, it only activates after the first default grid generation.
+// after that, for next grid generations through user input, it doesn't work. So to make the event activate after every grid generation, 
+// put the event inside a function and call the function after grid generation
+function activateHoverEffect() {
+    divColor = document.querySelectorAll('.columnDiv');
+    divColor.forEach(divColor => {
+        divColor.addEventListener("mouseover", function (e) {
+            e.target.style.backgroundColor = "Black";
+        });    
+    });
+}
+
+
+// event selectors
+
+//even when button is clicked
+const btn = document.querySelector('.changeSize');
+btn.addEventListener('click', alertSizeInput);
+
