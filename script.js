019baf8d02ac983164fa4218929fb673ae36614b
Abraham Justin <162578329+abrahamjust@users.noreply.github.com>
2024-10-28 06:44:59 +0000

    Added the logic to change background colour when mouse hovers over the divs

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const container = document.querySelector('.Container');
-for(i = 0; i < 10; i++) {
-    const rowDiv = document.createElement('div');
-    rowDiv.className = 'rowDiv';
-    for(j = 0; j < 10; j++) {
-        const colDiv = document.createElement('div');
-        colDiv.className = 'columnDiv';
-        rowDiv.appendChild(colDiv);
+function createGrid(a) {
+    const container = document.querySelector('.Container');
+    for(i = 0; i < a; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.className = 'rowDiv';
+        for(j = 0; j < a; j++) {
+            const colDiv = document.createElement('div');
+            colDiv.className = 'columnDiv';
+            rowDiv.appendChild(colDiv);
+        }
+        container.appendChild(rowDiv);
     }
-    container.appendChild(rowDiv);
 }
+
+a = 10;
+createGrid(a);
+
+divColor = document.querySelectorAll('.columnDiv');
+divColor.forEach(divColor => {
+    divColor.addEventListener("mouseover", function (e) {
+        e.target.style.backgroundColor = "Black";
+      });    
+});
